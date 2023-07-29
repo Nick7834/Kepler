e2704104e917a4155f1a32d05707dee14b8e7d5d
Nikita <Hellreg124@gmail.com>
2023-07-29 12:16:58 +0300

    post

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import App from '../App'
 import { PostDetail } from './display/PostDetail/PostDetail'
 
 
 export const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route element={<App />} path='/Kepler' />
             <Route element={<PostDetail />} path='/posts/:id' />
 
             <Route path='*' element={<div>Not posts</div>} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
